2b1ab18c034850884065f62a9bea793ab7ae3328
marinoszinonos <zinonos@icon.cy>
2025-05-30 22:47:16 +0300

    Refactor SwiftInmobiCmpPlugin.swift

--- ios/Classes/InmobiCmpPlugin.swift
+++ ios/Classes/InmobiCmpPlugin.swift
@@ -3,102 +3,111 @@ import UIKit
 import InMobiCMP
 
 public class SwiftInmobiCmpPlugin: NSObject, FlutterPlugin {
-  private var channel: FlutterMethodChannel?
-
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "com.icon.inmobi_cmp",
-                                       binaryMessenger: registrar.messenger())
-    let instance = SwiftInmobiCmpPlugin()
-    instance.channel = channel
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "init":
-      initConsent(call: call, result: result)
-    case "showConsent":
-      showConsent(result: result)
-    case "getConsentStatus":
-      getConsentStatus(result: result)
-    default:
-      result(FlutterMethodNotImplemented)
+    private var channel: FlutterMethodChannel?
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "com.icon.inmobi_cmp",
+                                           binaryMessenger: registrar.messenger())
+        let instance = SwiftInmobiCmpPlugin()
+        instance.channel = channel
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
-
-  private func initConsent(call: FlutterMethodCall, result: FlutterResult) {
-    guard let args = call.arguments as? [String: Any],
-          let pCode = args["pCode"] as? String else {
-      result(FlutterError(code: "INIT_ERROR", message: "Missing pCode", details: nil))
-      return
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "init":
+            initConsent(call: call, result: result)
+        case "showConsent":
+            showConsent(result: result)
+        case "getConsentStatus":
+            getConsentStatus(result: result)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
     }
-
-    // Start CMP
-    ChoiceCmp.shared.startChoice(pcode: pCode, delegate: self)
-    result(nil)
-  }
-
-  private func showConsent(result: FlutterResult) {
-    DispatchQueue.main.async {
-      ChoiceCmp.shared.forceDisplayUI()
-      result(nil)
+    
+    private func initConsent(call: FlutterMethodCall, result: FlutterResult) {
+        guard let args = call.arguments as? [String: Any],
+              let pCode = args["pCode"] as? String else {
+            result(FlutterError(code: "INIT_ERROR", message: "Missing pCode", details: nil))
+            return
+        }
+        
+        // Start CMP
+        ChoiceCmp.shared.startChoice(pcode: pCode, delegate: self)
+        result(nil)
     }
-  }
-
-  private func getConsentStatus(result: FlutterResult) {
-    ChoiceCmp.shared.getGDPRDataWithCompletion { gdprData in
-      if gdprData != nil {
-        result("gdpr_data_available")
-      } else if ChoiceCmp.shared.getNonIABData() != nil {
-        result("non_iab_data_available")
-      } else {
-        result("no_consent_data")
-      }
+    
+    private func showConsent(result: @escaping FlutterResult) {
+        DispatchQueue.main.async {
+            ChoiceCmp.shared.forceDisplayUI()
+            result(nil)
+        }
     }
-  }
-
-  private func sendLogToFlutter(_ message: String) {
-    DispatchQueue.main.async {
-      self.channel?.invokeMethod("onCmpEvent", arguments: message)
+    
+    private func getConsentStatus(result: @escaping FlutterResult) {
+        ChoiceCmp.shared.getGDPRData { gdprData in
+            // if tcString is non-empty, we have stored IAB-vendor data
+            if !gdprData.tcString.isEmpty {
+                result("gdpr_data_available")
+            }
+            // otherwise fall back to checking non-IAB data
+            else if let _ = ChoiceCmp.shared.getNonIABData() {
+                result("non_iab_data_available")
+            }
+            // and if neither has anything, report “no_consent_data”
+            else {
+                result("no_consent_data")
+            }
+        }
     }
-  }
+    
+    private func sendLogToFlutter(_ message: String) {
+        DispatchQueue.main.async {
+            self.channel?.invokeMethod("onCmpEvent", arguments: message)
+        }
+    }
 }
 
 // MARK: - ChoiceCmpDelegate
 extension SwiftInmobiCmpPlugin: ChoiceCmpDelegate {
-  public func didReceiveIABVendorConsent(gdprData: GDPRData, updated: Bool) {
-    sendLogToFlutter("IAB vendor consent given: updated=\(updated), tcString=\(gdprData.tcString)")
-  }
-
-  public func didReceiveNonIABVendorConsent(nonIabData: NonIABData, updated: Bool) {
-    sendLogToFlutter("Non-IAB vendor consent given: updated=\(updated), consent=\(nonIabData.consent)")
-  }
-
-  public func didReceiveAdditionalConsent(acData: ACData, updated: Bool) {
-    sendLogToFlutter("Google vendor consent given: updated=\(updated), acString=\(acData.acString)")
-  }
-
-  public func didReceiveCCPAConsent(string: String) {
-    sendLogToFlutter("CCPA Consent given: \(string)")
-  }
-
-  public func cmpDidError(error: Error) {
-    sendLogToFlutter("CMP error: \(error.localizedDescription)")
-  }
-
-  public func didReceiveUSRegulationsConsent(usRegData: USRegulationsData) {
-    sendLogToFlutter("US regulations consent received: gppString=\(usRegData.gppString)")
-  }
-
-  public func userDidMoveToOtherState() {
-    sendLogToFlutter("User moved to another state")
-  }
-
-  public func cmpUIStatusChanged(info: DisplayInfo) {
-    sendLogToFlutter("CMP UI status changed: \(info)")
-  }
-
-  public func didReceiveActionButtonTap(action: ActionButtons) {
-    sendLogToFlutter("Action button clicked: \(action.rawValue)")
-  }
+    public func cmpDidLoad(info: InMobiCMP.PingResponse) {
+        sendLogToFlutter("CMP did load: \(info)")
+    }
+    
+    public func didReceiveIABVendorConsent(gdprData: GDPRData, updated: Bool) {
+        sendLogToFlutter("IAB vendor consent given: updated=\(updated), tcString=\(gdprData.tcString)")
+    }
+    
+    public func didReceiveNonIABVendorConsent(nonIabData: NonIABData, updated: Bool) {
+        sendLogToFlutter("Non-IAB vendor consent given: updated=\(updated), consent=\(nonIabData.consent)")
+    }
+    
+    public func didReceiveAdditionalConsent(acData: ACData, updated: Bool) {
+        sendLogToFlutter("Google vendor consent given: updated=\(updated), acString=\(acData.acString)")
+    }
+    
+    public func didReceiveCCPAConsent(string: String) {
+        sendLogToFlutter("CCPA Consent given: \(string)")
+    }
+    
+    public func cmpDidError(error: Error) {
+        sendLogToFlutter("CMP error: \(error.localizedDescription)")
+    }
+    
+    public func didReceiveUSRegulationsConsent(usRegData: USRegulationsData) {
+        sendLogToFlutter("US regulations consent received: gppString=\(usRegData.gppString)")
+    }
+    
+    public func userDidMoveToOtherState() {
+        sendLogToFlutter("User moved to another state")
+    }
+    
+    public func cmpUIStatusChanged(info: DisplayInfo) {
+        sendLogToFlutter("CMP UI status changed: \(info)")
+    }
+    
+    public func didReceiveActionButtonTap(action: ActionButtons) {
+        sendLogToFlutter("Action button clicked: \(action.rawValue)")
+    }
 }
